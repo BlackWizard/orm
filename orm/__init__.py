0ad406039b0c67132f0b26fde211e30e25620003
BlackWizard <blackwizard@mail.ru>
2019-08-16 13:24:41 +0200

    [add] Enum field

--- orm/__init__.py
+++ orm/__init__.py
@@ -1,5 +1,5 @@
 from orm.exceptions import MultipleMatches, NoMatch
-from orm.fields import Boolean, Integer, Float, String, Text, Date, Time, DateTime, JSON, ForeignKey
+from orm.fields import Boolean, Integer, Float, String, Text, Date, Time, DateTime, JSON, ForeignKey, Enum
 from orm.models import Model
 
 __version__ = "0.1.4"
